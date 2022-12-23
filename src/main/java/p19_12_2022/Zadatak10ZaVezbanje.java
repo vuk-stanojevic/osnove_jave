d689de86ab811825e6b2f8769a3a13ea840cf205
Vuk Stanojevic <vukstanojevic42@gmail.com>
2022-12-23 17:41:22 +0100

    Predavanja 19.12.2022.

---
+++ src/main/java/p19_12_2022/Zadatak10ZaVezbanje.java
@@ -0,0 +1,28 @@
+package p19_12_2022;
+
+import java.util.Scanner;
+
+public class Zadatak10ZaVezbanje {
+
+    public static void main(String[] args) {
+
+//        (Za vezbanje) Napisati program koji za sve petocifene brojeve radi mirror opciju.
+//        Primer izvrsenja:
+//        Unesite broj: 32492
+//        Novi broj je 29423
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Unesite broj: ");
+        int broj = s.nextInt();
+
+        String mirror = "";
+
+        for (int i = 0; i < 5; i++) {
+            int cifra = broj%10;
+            mirror = mirror + cifra;
+            broj=broj/10;
+        }
+        System.out.println("Novi broj je " + mirror);
+    }
+}
