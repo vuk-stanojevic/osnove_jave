b59cf29b7061708bdae69b7b295fb74a0dca8ab9
Vuk Stanojevic <vukstanojevic42@gmail.com>
2022-12-23 17:33:50 +0100

    Predavanja 20.12.2022.

--- src/main/java/p20_12_2022/Zadatak6ZaVezbanje.java
+++ src/main/java/p20_12_2022/Zadatak6ZaVezbanje.java
@@ -1,5 +1,7 @@
 package p20_12_2022;
 
+import java.util.Scanner;
+
 public class Zadatak6ZaVezbanje {
     public static void main(String[] args) {
 //        (ZA VEZBANJE) Napisati program koji izvlaci cifre iz visecifrenog broja.
@@ -8,5 +10,16 @@ public class Zadatak6ZaVezbanje {
 //Primer izvrsenja:
 //Unesite broj: 2109349
 //9, 4, 3, 0, 9, 1, 2
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Unesite broj sa najvise 10 cifara: ");
+        int broj = s.nextInt();
+
+        while(broj>=0){
+            int cifra = broj % 10;
+            System.out.print(cifra + ", ");
+            broj = broj/10;
+        }
     }
 }
